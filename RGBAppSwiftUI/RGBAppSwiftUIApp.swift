af316542475c42b7d037b9ba4ef2364defe0660a
Alex Petrenko <alex.pt@ya.ru>
2023-12-01 22:26:14 +0300

    Initial Commit

---
+++ RGBAppSwiftUI/RGBAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppSwiftUIApp.swift
+//  RGBAppSwiftUI
+//
+//  Created by Алексей on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
